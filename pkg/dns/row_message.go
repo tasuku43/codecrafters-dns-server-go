4715cd88e9db29185e5f3944579e572939be8dbd
tasuku43 <task.teckac@gmail.com>
2024-01-21 05:52:29 +0900

    refactor: RawMessage and RawQuestions Integration.

--- pkg/dns/row_message.go
+++ pkg/dns/row_message.go
@@ -15,8 +15,6 @@ type RowName []byte
 
 type RowQuestion []byte
 
-type RowQuestions []byte
-
 type RawMessage []byte
 
 func (h RowHeaderFlags) parse() HeaderFlags {
@@ -80,21 +78,23 @@ func (q RowQuestion) parse() Question {
 	}
 }
 
-func (qs RowQuestions) parse() (Questions, error) {
-	var questions Questions
-	offset := 0
+func (p RawMessage) Parse() (Message, error) {
+	header := RowHeader(p[0:12]).parse()
 
-	for offset < len(qs) {
-		labelOffset, err := findLabelPointerOffset(offset, qs[offset:])
+	var questions Questions
+	offset := 12
+
+	for offset < len(p) {
+		labelOffset, err := findLabelPointerOffset(offset, p[offset:])
 		if labelOffset.labelEndOffset > 0 {
 			endOfName := labelOffset.labelEndOffset
 			endOfQuestion := offset + endOfName + 5
 
-			if endOfQuestion > len(qs) {
-				return nil, fmt.Errorf("invalid question length")
+			if endOfQuestion > len(p) {
+				return Message{}, fmt.Errorf("invalid question length")
 			}
 
-			questions = append(questions, RowQuestion(qs[offset:endOfQuestion]).parse())
+			questions = append(questions, RowQuestion(p[offset:endOfQuestion]).parse())
 
 			offset = endOfQuestion
 
@@ -102,36 +102,28 @@ func (qs RowQuestions) parse() (Questions, error) {
 		}
 		if labelOffset.compressionPointerOffset > 0 {
 			pointerOffset := labelOffset.compressionPointerOffset - 12
-			endOfName, _ := findNullOffset(qs[pointerOffset:])
+			endOfName, _ := findNullOffset(p[pointerOffset:])
 
 			var name RowName
-			name = append(name, qs[offset:labelOffset.value-1]...)
-			name = append(name, qs[pointerOffset:pointerOffset+endOfName+1]...)
+			name = append(name, p[offset:labelOffset.value-1]...)
+			name = append(name, p[pointerOffset:pointerOffset+endOfName+1]...)
 
 			endOfQuestion := labelOffset.value + 5
 
 			var rq RowQuestion
 			rq = append(rq, name...)
-			rq = append(rq, qs[labelOffset.value+1:endOfQuestion]...)
+			rq = append(rq, p[labelOffset.value+1:endOfQuestion]...)
 			questions = append(questions, rq.parse())
 
 			offset = endOfQuestion
 
 			continue
 		}
-		return Questions{}, err
-	}
-
-	return questions, nil
-}
-
-func (p RawMessage) Parse() (Message, error) {
-	qs, err := RowQuestions(p[12:]).parse()
-	if err != nil {
 		return Message{}, err
 	}
+
 	return Message{
-		Header:    RowHeader(p[0:12]).parse(),
-		Questions: qs,
+		Header:    header,
+		Questions: questions,
 	}, nil
 }
